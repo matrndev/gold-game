f26c2291a1452dbc105da92db3c3ccee66b7e010
Matyáš Trnka <hello@matrn.dev>
2026-02-14 22:05:39 +0100

    add miner speed variable

--- scripts/miner.gd
+++ scripts/miner.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @export var miner_id: int = -1
+@export var miner_speed: float = 1.0
 
 signal mined
 
 func _ready() -> void:
 	$Label.text = "miner " + str(miner_id)
+	$MineTimer.wait_time = miner_speed
 
 
 func _on_mine_timer_timeout() -> void:
