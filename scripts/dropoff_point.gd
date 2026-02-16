1b199b48030ee3838cd6e142dbef0691ec92f755
Matyáš Trnka <hello@matrn.dev>
2026-02-17 00:17:43 +0100

    drop one by one + adjust drop speed

--- scripts/dropoff_point.gd
+++ scripts/dropoff_point.gd
@@ -3,15 +3,19 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print(body.name)
 	if body.name == "Player":
 		body.modulate = Color(0.001, 0.526, 1.0, 1.0)
-		if StoredStats.gold_inventory > 0:
-			print("dropped ", StoredStats.gold_inventory)
-			StoredStats.stored_gold += StoredStats.gold_inventory
-			StoredStats.gold_inventory = 0
+		$DropSpeed.start()
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		body.modulate = Color(1.0, 1.0, 1.0, 1.0)
+		$DropSpeed.stop()
+
+
+func _on_drop_speed_timeout() -> void:
+	if StoredStats.gold_inventory > 0:
+		StoredStats.stored_gold += 1
+		StoredStats.gold_inventory -= 1
+		StoredStats.gold_transactions.append(-1)
