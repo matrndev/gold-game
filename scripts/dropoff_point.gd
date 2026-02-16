16335766741ba2e832a5c1f72a76bbaa464be196
Matyáš Trnka <hello@matrn.dev>
2026-02-16 22:07:34 +0100

    fix buggy scene manager (scenes weren't fully switching)

--- scripts/dropoff_point.gd
+++ scripts/dropoff_point.gd
@@ -3,8 +3,15 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
+	print(body.name)
 	if body.name == "Player":
+		body.modulate = Color(0.001, 0.526, 1.0, 1.0)
 		if StoredStats.gold_inventory > 0:
 			print("dropped ", StoredStats.gold_inventory)
 			StoredStats.stored_gold += StoredStats.gold_inventory
 			StoredStats.gold_inventory = 0
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		body.modulate = Color(1.0, 1.0, 1.0, 1.0)
--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -8,30 +8,33 @@ func _ready() -> void:
 	mining_facility_scene = load("res://scenes/game.tscn").instantiate()
 	get_tree().root.call_deferred("add_child", mining_facility_scene)
 
+func mining_scene_active(active: bool) -> void:
+	# this below doesnt work, keep it disabled!
+	# mining_facility_scene.process_mode = Node.PROCESS_MODE_INHERIT if active else Node.PROCESS_MODE_DISABLED # this is apparently something resembling a ternary operator in gdscript lol 
+	
+	for area in mining_facility_scene.find_children("*", "Area2D", true, false): # disable collisions with teleports and gold mines
+		area.set_deferred("monitoring", active)
+		area.set_deferred("monitorable", active)
+	
+	# retain the player, but hide everything else
+	for child in mining_facility_scene.get_children():
+		if child.name != "Player":
+			child.visible = active
+
 func go_to(path: String) -> void:
-	#if path == "res://scenes/game.tscn":
-		#if active_scene:
-			#active_scene.queue_free()
-			#active_scene = null
-		#mining_facility_scene.visible = true
-	#else:
-		#mining_facility_scene.visible = false
-		#if active_scene:
-			#active_scene.queue_free()
-			#active_scene = null
-		#active_scene = load(path).instantiate()
-		#get_tree().root.call_deferred("add_child", active_scene)
-		
 	if path == "res://scenes/game.tscn":
 		if active_scene:
 			active_scene.queue_free()
+			print("clearing act sc")
 			active_scene = null
-		mining_facility_scene.visible = true
+		mining_scene_active(true)
 	else:
-		mining_facility_scene.visible = false
+		mining_scene_active(false)
 		if active_scene:
 			active_scene.queue_free()
+			print("clearing act sc")
 			active_scene = null
 		active_scene = load(path).instantiate()
+		print("loading ", active_scene.name)
 		get_tree().root.call_deferred("add_child", active_scene)
 		
