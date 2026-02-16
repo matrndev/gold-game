39b64036bcaeb5ee2282d7004318e750de1e7038
Matyáš Trnka <hello@matrn.dev>
2026-02-17 00:18:09 +0100

    Update teleport.gd

--- scripts/teleport.gd
+++ scripts/teleport.gd
@@ -15,8 +15,6 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		match go_to:
 			Locations.DROPOFF_MAZE:
-				$ColorRect/Label.text = "Teleport to Dropoff Destination"
 				SceneManager.go_to("res://scenes/dropoff_maze.tscn")
 			Locations.MINING_FACILITY:
-				$ColorRect/Label.text = "Teleport to Mining Facility"
 				SceneManager.go_to("res://scenes/game.tscn")
